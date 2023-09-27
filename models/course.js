4dbad84a14f4b79dd6fe337b66f2945f20d11ea0
Andika <andikaraditya1@gmail.com>
2023-09-27 17:44:47 +0700

    Migration and seeding

--- models/course.js
+++ models/course.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Course.belongsToMany(models.User, {through: models.UserCourse})
     }
   }
   Course.init({
