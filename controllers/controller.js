b8721fb0e7aa3bbfd11d0c0053c9652cdb5c41e3
Andika <andikaraditya1@gmail.com>
2023-09-28 09:08:04 +0700

    Add user profile page

--- controllers/controller.js
+++ controllers/controller.js
@@ -153,6 +153,71 @@ class Controller {
             console.log(err)
             res.send(err)
         })
+    static profilePage(req, res) {
+        const {id} = req.params
+        User.findByPk(id, {
+            include: [UserProfile, Role]
+        })
+            .then((result) => {
+                // res.send(result)
+                res.render("profile-page", {result})
+            })
+            .catch((err) => {
+                console.log(err)
+                res.send(err)
+            })
+    }
+
+    static editProfilePage(req, res) {
+        const {id} = req.params
+        let user
+        User.findByPk(id)
+            .then((result) => {
+                user = result
+                return UserProfile.findOne({
+                    where: {
+                        UserId: id
+                    }
+                })
+            })
+            .then((userProfile) => {
+                // res.send({user, userProfile})
+                res.render("profile-edit", {user, userProfile})
+            })
+            .catch((err) => {
+                console.log(err)
+                res.send(err)
+            })
+    }
+
+    static updateProfile(req, res) {
+        const {id} = req.params
+        const {firstName, lastName, email, password, dateOfBirth, education} = req.body
+        User.update({
+            email,
+            password
+        },{
+            where: {
+                id:id
+            }
+        })
+            .then((result) => {
+                return UserProfile.update({
+                    firstName,
+                    lastName,
+                    dateOfBirth,
+                    education
+                },{
+                    where: {
+                        UserId: id
+                    }
+                })
+            })
+            .then((result) => res.redirect(`/profile/${id}`))
+            .catch((err) => {
+                console.log(err)
+                res.send(err)
+            })
     }
 }
 
