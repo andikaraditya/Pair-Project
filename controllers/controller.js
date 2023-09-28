f419d52f93996bd07bce9ad37be98a1a1f293df0
Andika <andikaraditya1@gmail.com>
2023-09-28 07:58:50 +0700

    Login & Register

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,31 @@
 const {Course, Role, User, UserCourse, UserProfile} = require('../models');
+const bcrypt = require('bcryptjs')
 
 class Controller {
 
     static loginPage(req, res) {
         res.render("login-page")
+    }
+
+    static loginAccount(req, res) {
+        const {email, password} = req.body
+        User.findOne({
+            where: {
+                email: email
+            }
+        })
+            .then((account) => {
+                const isCorrect = bcrypt.compareSync(password, account.password)
+                if (isCorrect) {
+                    res.send(account)
+                } else {
+                    res.send("Wrong Password")
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+                res.send(err)
+            })
     }
 
     static registerPage(req, res) {
